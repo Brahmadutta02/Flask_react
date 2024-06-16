e64a2e7a4fec0dfd0aee20ef53bcb0d337cd2e60
brahmadutta02 <brahmaduttadalai012@gmail.com>
2024-06-16 09:20:16 +0530

    adding chapter 2 part 1

---
+++ frontend/Chapter_2/part_1/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import SpeakerProfile from './speakerProfile';
+
+function App(){
+      return (
+    <div style={{backgroundColor: 'gray', margin:20, color:'white'}}>
+      <SpeakerProfile  />
+    </div>
+  );
+}
+
+export default App;
