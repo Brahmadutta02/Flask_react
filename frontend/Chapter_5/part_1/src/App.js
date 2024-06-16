6cd7580ad83aab03cf341f2d593fc22e19bcd68f
brahmadutta02 <brahmaduttadalai012@gmail.com>
2024-06-16 22:45:59 +0530

    adding chapter 5 part 1

---
+++ frontend/Chapter_5/part_1/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App(){
+    const speakerName = "Brehms"
+    return(
+        <div className='App'>
+            <h2>{speakerName}</h2>
+            <h2>{5+5}</h2>
+        </div>
+    )
+}
