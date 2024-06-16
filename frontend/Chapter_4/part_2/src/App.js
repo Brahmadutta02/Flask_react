9daf980cf75282e6e300de1f4dc05a4e0f650b11
brahmadutta02 <brahmaduttadalai012@gmail.com>
2024-06-16 21:36:32 +0530

    adding chapter 4 part 2

---
+++ frontend/Chapter_4/part_2/src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+
+const App = () => {
+    const [data, setData] = useState([]);
+
+    const getSpeakers = ()=>{
+        fetch("https://jsonplaceholder.typicode.com/users")
+           .then((response) => response.json())
+           .then((data) => {
+             setData( data);
+           })
+        }
+        useEffect(() => {
+          getSpeakers()
+        },[]);
+        return (
+          <>
+            <h1>Displaying Speakers Information</h1>
+            <ul>
+              {data.map(speaker => (
+                <li key={speaker.id}>
+                  {speaker.name},  <em> {speaker.email} </em>
+                </li>
+              ))}
+            </ul>
+          </>
+        );
+    };
+
+export default App;
